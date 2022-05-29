9e298d7000f65e2d5a060c22bedf5f832b42d87c
erzetid <fahrizalm14@gmail.com>
2022-05-30 02:29:24 +0700

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Copyright (c) 2022 fahrizalm14
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export const hello = (name: string): string => `Hello ${name}`;
