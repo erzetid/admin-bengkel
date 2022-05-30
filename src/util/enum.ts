3e40c3490533ec25c538d6e3f79641391a92dfe6
erzetid <fahrizalm14@gmail.com>
2022-05-30 23:24:43 +0700

    Add enum

---
+++ src/util/enum.ts
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 fahrizalm14
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+export enum StatusPayment {
+  PENDING = "PENDING",
+  PAID = "PAID",
+  UNPAID = "UNPAID",
+  CANCELED = "CANCELED",
+}
