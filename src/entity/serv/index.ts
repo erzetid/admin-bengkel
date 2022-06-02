d4582651302df8c8996cadde2cd10b0f2818e70a
erzetid <fahrizalm14@gmail.com>
2022-06-03 00:26:42 +0700

    export abstract serv

--- src/entity/serv/index.ts
+++ src/entity/serv/index.ts
@@ -3,7 +3,10 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+import Serv from "../../../dist/entity/serv/Serv";
+
 export { FullService } from "./FullService";
 export { LightService } from "./LightService";
 export { RepairService } from "./RepairService";
 export { ReplaceService } from "./ReplaceService";
+export { Serv };
