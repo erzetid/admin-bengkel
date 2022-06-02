415460608f5acf49950b2c6ea34ee113a3df6dcb
erzetid <fahrizalm14@gmail.com>
2022-06-03 00:25:10 +0700

    set module cashflow

---
+++ src/entity/cashflow/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 fahrizalm14
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import CashFlow from "./CashFlow";
+
+export { CashFlow };
+export { DirectCash } from "./DirectCash";
+export { PartCash } from "./PartCash";
+export { TransactionCash } from "./TransactionCash";
