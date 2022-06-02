f1b685d36b64f077df5c421d98fb000237b74cab
erzetid <fahrizalm14@gmail.com>
2022-06-03 00:25:45 +0700

    register account and cashflow class

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+export * from "./entity/account";
 export * from "./entity/balance";
+export * from "./entity/cashflow";
 export * from "./entity/costumer";
 export * from "./entity/part";
 export * from "./entity/report";
